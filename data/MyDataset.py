922cfb5ab175963fcee4f3d79a1eebdb877b9472
ClumsyInvoker <50040713+ClumsyInvoker@users.noreply.github.com>
2024-04-23 23:00:20 +0800

    add CDN

--- data/MyDataset.py
+++ data/MyDataset.py
@@ -85,7 +85,7 @@ class MyDataset(Dataset):
     def __init__(self, data_dir, max_length=50, mode='train', neg_num=1, device='cpu'):
         self.mode = mode
         self.max_length = max_length
-        self.neg_num = PTCRDataset
+        self.neg_num = neg_num
         self.device = device
 
         # 从CSV文件中加载meta data
@@ -371,6 +371,8 @@ class PLATEDataset(Dataset):
 
         # 加载冷启动item信息
         self.cold_item_ids = pd.read_pickle(data_dir + '/cold_item_ids.pkl')
+        self.cold_item_ids_tensor = torch.zeros(self.item_num, dtype=torch.long)
+        self.cold_item_ids_tensor[self.cold_item_ids] = 1
 
 
     def __len__(self):
@@ -430,10 +432,11 @@ class PLATEDataset(Dataset):
             item_features = torch.FloatTensor(item_features.values).to(self.device)
             neg_item_features = torch.FloatTensor(neg_item_features.values).to(self.device)
 
-            is_cold_items = torch.LongTensor([1 if target_item_id in self.cold_item_ids else 0] +
-                                                  [1 if neg_item_id in self.cold_item_ids else 0
-                                                   for neg_item_id in neg_item_ids]).to(self.device)
-            is_cold_items = is_cold_items.unsqueeze(1)
+            # is_cold_items = torch.LongTensor([1 if target_item_id in self.cold_item_ids else 0] +
+            #                                        [1 if neg_item_id in self.cold_item_ids else 0
+            #                                         for neg_item_id in neg_item_ids]).to(self.device)
+            is_cold_items = self.cold_item_ids_tensor[[target_item_id] + neg_item_ids]
+            is_cold_items = is_cold_items.unsqueeze(1).to(self.device)
 
             # 返回样本
             return user_id, history_items, history_items_len, \
@@ -480,6 +483,8 @@ class MetaEmbDataset(Dataset):
 
         self.cold_item_ids = pd.read_pickle(data_dir + '/cold_item_ids.pkl')
         self.hot_item_ids = [i for i in range(self.item_num) if i not in self.cold_item_ids]
+        self.cold_item_ids_tensor = torch.zeros(self.item_num, dtype=torch.long)
+        self.cold_item_ids_tensor[self.cold_item_ids] = 1
 
 
     def __len__(self):
@@ -511,14 +516,36 @@ class MetaEmbDataset(Dataset):
             user_a_features = self.user_features.iloc[user_a]
             user_b_features = self.user_features.iloc[user_b]
             item_features = self.item_features.iloc[target_item_id]
-            raw_history_items = []
+            history_items_a = []
+            history_items_len_a = []
+            for user in user_a:
+                raw_history_item = self.user_history_positive[user][-self.max_length:]
+                history_items_a.append(raw_history_item + [0] * (self.max_length - len(raw_history_item)))
+                history_items_len_a.append(
+                    [1] * len(raw_history_item) + [0] * (self.max_length - len(raw_history_item)))
+            history_items_b = []
+            history_items_len_b = []
+            for user in user_b:
+                raw_history_item = self.user_history_positive[user][-self.max_length:]
+                history_items_b.append(raw_history_item + [0] * (self.max_length - len(raw_history_item)))
+                history_items_len_b.append(
+                    [1] * len(raw_history_item) + [0] * (self.max_length - len(raw_history_item)))
 
             user_a_ids = torch.LongTensor(user_a).to(self.device)
             user_b_ids = torch.LongTensor(user_b).to(self.device)
-            history_items = torch.LongTensor(raw_history_items + [0] * (self.max_length - len(raw_history_items))).to(
-                self.device)
-            history_items_len = torch.LongTensor(
-                [1] * len(raw_history_items) + [0] * (self.max_length - len(raw_history_items))).to(self.device)
+
+            history_items_a = torch.LongTensor(history_items_a).to(self.device)
+            history_items_len_a = torch.LongTensor(history_items_len_a).to(self.device)
+            history_items_b = torch.LongTensor(history_items_b).to(self.device)
+            history_items_len_b = torch.LongTensor(history_items_len_b).to(self.device)
+            # history_items_a = torch.LongTensor(raw_history_items_a + [0] * (self.max_length - len(raw_history_items_a))).to(
+            #     self.device)
+            # history_items_len_a = torch.LongTensor(
+            #     [1] * len(raw_history_items_a) + [0] * (self.max_length - len(raw_history_items_a))).to(self.device)
+            # history_items_b = torch.LongTensor(raw_history_items_b + [0] * (self.max_length - len(raw_history_items_b))).to(
+            #     self.device)
+            # history_items_len_b = torch.LongTensor(
+            #     [1] * len(raw_history_items_b) + [0] * (self.max_length - len(raw_history_items))).to(self.device)
             target_item_ids = torch.LongTensor([target_item_id]).to(self.device)
             user_a_features = torch.FloatTensor(user_a_features.values).to(self.device)
             user_b_features = torch.FloatTensor(user_b_features.values).to(self.device)
@@ -527,8 +554,8 @@ class MetaEmbDataset(Dataset):
             label_b = torch.FloatTensor([total_label[i] for i in idx_b]).to(self.device)
             cold_item = torch.FloatTensor([0]).to(self.device)
 
-            return user_a_ids, user_b_ids, history_items, history_items_len, target_item_ids,\
-                user_a_features, user_b_features, item_features, label_a, label_b, cold_item
+            return user_a_ids, user_b_ids, history_items_a, history_items_len_a, history_items_b, history_items_len_b,\
+                   target_item_ids, user_a_features, user_b_features, item_features, label_a, label_b, cold_item
 
         elif self.mode == 'val' or self.mode == 'test':
             row = self.data.iloc[idx]
@@ -558,10 +585,121 @@ class MetaEmbDataset(Dataset):
             item_features = torch.FloatTensor(item_features.values).to(self.device)
             neg_item_features = torch.FloatTensor(neg_item_features.values).to(self.device)
 
-            is_cold_items = torch.LongTensor([1 if target_item_id in self.cold_item_ids else 0] +
-                                                  [1 if neg_item_id in self.cold_item_ids else 0
-                                                   for neg_item_id in neg_item_ids]).to(self.device)
-            is_cold_items = is_cold_items.unsqueeze(1)
+            # is_cold_items = torch.LongTensor([1 if target_item_id in self.cold_item_ids else 0] +
+            #                                        [1 if neg_item_id in self.cold_item_ids else 0
+            #                                         for neg_item_id in neg_item_ids]).to(self.device)
+            is_cold_items = self.cold_item_ids_tensor[[target_item_id] + neg_item_ids]
+            is_cold_items = is_cold_items.unsqueeze(1).to(self.device)
+
+            # 返回样本
+            return user_id, history_items, history_items_len, \
+                target_item_id, neg_item_id, \
+                user_features, item_features, neg_item_features, \
+                is_cold_items
+
+        else:
+            raise ValueError('mode must be train/val/test')
+
+
+class CDNDataset(Dataset):
+    def __init__(self, data_dir, max_length=50, mode='train', neg_num=1, device='cpu'):
+        self.mode = mode
+        self.max_length = max_length
+        self.neg_num = PTCRDataset
+        self.device = device
+
+        # 从CSV文件中加载meta data
+        meta_data = pd.read_csv(data_dir + '/meta_data.csv')
+        self.name = meta_data['dataset_name'].values[0]
+        self.user_num = meta_data['user_num'].values[0]
+        self.item_num = meta_data['item_num'].values[0]
+
+        # 加载用户和物品的特征
+        self.user_features = pd.read_csv(data_dir + '/user_features.csv')
+        self.user_features_dim = self.user_features.shape[1]
+        self.item_features = pd.read_csv(data_dir + '/item_features.csv')
+        self.item_features_dim = self.item_features.shape[1]
+
+        # 加载交互数据
+        if self.mode == 'train_cold':
+            self.data = pd.read_csv(data_dir + '/train_data_cold.csv')
+        else:
+            self.data = pd.read_csv(data_dir + '/' + mode + '_data.csv')
+        # self.user_history = pd.read_pickle(data_dir + '/user_history.pkl')
+        # self.item_history = pd.read_pickle(data_dir + '/item_history.pkl')
+        self.user_history_positive = pd.read_pickle(data_dir + '/user_history_positive.pkl')
+        self.user_history_negative = pd.read_pickle(data_dir + '/user_history_negative.pkl')
+        self.item_history_positive = pd.read_pickle(data_dir + '/item_history_positive.pkl')
+        self.item_history_negative = pd.read_pickle(data_dir + '/item_history_negative.pkl')
+
+        # 如果是test或val，则为每个样本生成negative item, 固定为100个
+        if self.mode == 'val' or self.mode == 'test':
+            self.data_neg_items = pd.read_pickle(data_dir + '/' + mode + '_data_neg_items.pkl')
+
+        # 加载冷启动item信息
+        self.cold_item_ids = pd.read_pickle(data_dir + '/cold_item_ids.pkl')
+        self.cold_item_ids_tensor = torch.zeros(self.item_num, dtype=torch.long)
+        self.cold_item_ids_tensor[self.cold_item_ids] = 1
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # 获取指定索引处的数据
+        row = self.data.iloc[idx]
+        user_id = int(row['user_id'])
+        target_item_id = int(row['item_id'])
+        positive_behavior_offset = int(row['positive_behavior_offset'])
+        label = row['label']
+        cold_item = row['cold_item']
+
+        raw_history_items = self.user_history_positive[user_id][
+                        max(0, positive_behavior_offset + 1 - self.max_length):positive_behavior_offset + 1]
+        user_features = self.user_features.iloc[user_id]
+        item_features = self.item_features.iloc[target_item_id]
+
+        if self.mode == 'train' or self.mode == 'train_cold':
+            # 转化成tensor
+            user_id = torch.LongTensor([user_id]).to(self.device)
+            history_items = torch.LongTensor(raw_history_items + [0] * (self.max_length - len(raw_history_items))).to(
+                self.device)
+            history_items_len = torch.LongTensor(
+                [1] * len(raw_history_items) + [0] * (self.max_length - len(raw_history_items))).to(self.device)
+            target_item_id = torch.LongTensor([target_item_id]).to(self.device)
+            user_features = torch.FloatTensor(user_features.values).to(self.device)
+            item_features = torch.FloatTensor(item_features.values).to(self.device)
+            label = torch.FloatTensor([label]).to(self.device)
+            cold_item = torch.FloatTensor([cold_item]).to(self.device)
+
+            return user_id, history_items, history_items_len, target_item_id,\
+                user_features, item_features, label, cold_item
+
+        elif self.mode == 'val' or self.mode == 'test':
+            # neg_item_ids = []
+            # if self.mode == 'val':
+            #     total_history_items = self.user_history_positive[user_id][:-1]
+            # else:
+            #     total_history_items = self.user_history_positive[user_id]
+            # for _ in range(self.neg_num):
+            #     neg_item_id = random_neq(0, self.item_num, set(total_history_items + neg_item_ids))
+            #     neg_item_ids.append(neg_item_id)
+            neg_item_ids = self.data_neg_items[idx]
+            neg_item_features = self.item_features.iloc[neg_item_ids]
+
+            # 转化成tensor
+            user_id = torch.LongTensor([user_id]).to(self.device)
+            history_items = torch.LongTensor(raw_history_items + [0] * (self.max_length - len(raw_history_items))).to(
+                self.device)
+            history_items_len = torch.LongTensor(
+                [1] * len(raw_history_items) + [0] * (self.max_length - len(raw_history_items))).to(self.device)
+            target_item_id = torch.LongTensor([target_item_id]).to(self.device)
+            neg_item_id = torch.LongTensor(neg_item_ids).to(self.device)
+            user_features = torch.FloatTensor(user_features.values).to(self.device)
+            item_features = torch.FloatTensor(item_features.values).to(self.device)
+            neg_item_features = torch.FloatTensor(neg_item_features.values).to(self.device)
+            is_cold_items = self.cold_item_ids_tensor[[target_item_id] + neg_item_ids]
+            is_cold_items = is_cold_items.unsqueeze(1).to(self.device)
 
             # 返回样本
             return user_id, history_items, history_items_len, \
